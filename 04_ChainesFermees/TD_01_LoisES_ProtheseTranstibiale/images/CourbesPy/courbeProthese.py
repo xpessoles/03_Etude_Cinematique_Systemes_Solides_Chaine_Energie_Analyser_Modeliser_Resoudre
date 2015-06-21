5a91ba57f666789fd2eed2a6f8cf83631ebda46a
Xavier Pessoles <xpessoles.ptsi@free.fr>
2015-06-21 15:14:08 +0200

    MAJ du TD

--- 04_ChainesFermees/TD_01_LoisES_ProtheseTranstibiale/images/CourbesPy/courbeProthese.py
+++ 04_ChainesFermees/TD_01_LoisES_ProtheseTranstibiale/images/CourbesPy/courbeProthese.py
@@ -12,8 +12,9 @@ import numpy as np
 
 a=0.117
 b=0.039
-x=linspace(-25,15,200)
-plt.plot(x,1000.*sqrt(b*b+a*a+2*a*b*sin(x*math.pi/180)))
+x=np.linspace(-25,15,200)
+y=1000.*np.sqrt(b*b+a*a+2*a*b*np.sin(x*np.math.pi/180))
+plt.plot(x,y)
 plt.ylabel("Course du vérin $\\lambda$ (en mm)")
 plt.xlabel("Angle $\\alpha$ (en degrés)")
 plt.grid()
